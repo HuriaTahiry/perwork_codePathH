14cff55672d0c73166ba71fdd9252b91282b1245
HuriaTahiry <126624992+HuriaTahiry@users.noreply.github.com>
2025-01-31 19:54:10 -0500

    Initial commit

---
+++ codepathPHW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codepathPHW2
+//
+//  Created by Huria Tahiry on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ codepathPHW2Tests/codepathPHW2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  codepathPHW2Tests.swift
+//  codepathPHW2Tests
+//
+//  Created by Huria Tahiry on 1/31/25.
+//
+
+import Testing
+@testable import codepathPHW2
+
+struct codepathPHW2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
